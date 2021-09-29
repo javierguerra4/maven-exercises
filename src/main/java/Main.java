32115db72b7b6882abd433637adf0c58e1da83e1
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-29 09:30:00 -0500

    Attempting to use StringUtils for first time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter something: ");
+
+        String userInput = myObj.nextLine();
+        System.out.println(userInput + );
+    }
+    public static String isNumeric() {
+        if (StringUtils.isNumeric(userInput))
+    }
+
 }
