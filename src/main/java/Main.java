a1385ad1e8aef0d9cdbdf98a1966bffb569a3532
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-29 10:53:42 -0500

    First time practicing test with Junit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter something: ");
-
-        String userInput = myObj.nextLine();
-        System.out.println(userInput + );
-    }
-    public static String isNumeric() {
-        if (StringUtils.isNumeric(userInput))
-    }
+//    public static void main(String[] args) {
+//        Scanner myObj = new Scanner(System.in);
+//        System.out.println("Enter something: ");
+//
+//        String userInput = myObj.nextLine();
+//        System.out.println(userInput + );
+//    }
+//    public static String isNumeric() {
+//        if (StringUtils.isNumeric(userInput))
+//    }
 
 }
